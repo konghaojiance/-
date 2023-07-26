518a4e6953c87ba2c0f1b5ede22ebd6301e33d9c
wang <piziyu@qq.com>
2023-07-27 00:31:39 +0800

    初始化

---
+++ src/main/java/cn/bk41/konghao/KonghaoApplication.java
@@ -0,0 +1,13 @@
+package cn.bk41.konghao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonghaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonghaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bk41/konghao/KonghaoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bk41.konghao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonghaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
